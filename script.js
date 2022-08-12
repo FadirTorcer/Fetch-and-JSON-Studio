078c93c80871aa3cba9f65289fdb7296e9f1b9d4
FadirTorcer <eric.lyonford@gmail.com>
2022-08-11 20:07:52 -0500

    Works, but overwrites each astronaut until Jeanette

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ window.addEventListener("load", function(){
     // TODO: fetch planets JSON
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then( function(json){
-            console.log(json);
-            console.log(json.length);
+            /* console.log(json);
+            console.log(json.length); */
             const destination = document.getElementById("container");
             for (let index = 0; index < json.length; index++) {
                 destination.innerHTML = `
                     <div class="astronaut">
                         <div class="bio">
-                            <h3>Mae Jemison</h3>
+                            <h3>${json[index].firstName} ${json[index].lastName}</h3>
                             <ul>
-                                <li>Hours in space: 190</li>
-                                <li>Active: false</li>
-                                <li>Skills: Physician, Chemical Engineer</li>
+                                <li>Hours in space: ${json[index].hoursInSpace}</li>
+                                <li>Active: ${json[index].active}</li>
+                                <li>Skills: ${json[index].skills}</li>
                             </ul>
                         </div>
-                        <img class="avatar" src="images/mae-jemison.jpg">
+                        <img class="avatar" src=${json[index].picture}>
                     </div>
                 `;
-            });
+            }
         });
     });
 });
