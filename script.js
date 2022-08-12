df93e12e7fe3a175695d57b89bc97e888dd961b6
FadirTorcer <eric.lyonford@gmail.com>
2022-08-11 19:48:20 -0500

    7:48 commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    // TODO: fetch planets JSON
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then( function(json){
+            console.log(json);
+            console.log(json.length);
+            const destination = document.getElementById("container");
+            for (let index = 0; index < json.length; index++) {
+                destination.innerHTML = `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>Mae Jemison</h3>
+                            <ul>
+                                <li>Hours in space: 190</li>
+                                <li>Active: false</li>
+                                <li>Skills: Physician, Chemical Engineer</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="images/mae-jemison.jpg">
+                    </div>
+                `;
+            });
+        });
+    });
+});
